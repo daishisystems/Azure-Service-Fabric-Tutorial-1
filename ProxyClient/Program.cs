1404607435c15f86cc90a17df14315c2959e27aa
Paul Mooney <paul@daishisystems.com>
2015-12-11 13:52:14 +0000

    initial commit

---
+++ ProxyClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using SimpleActor.Interfaces;
+
+namespace ProxyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var actorId = ActorId.NewId();
+
+            var simpleActor =
+                ActorProxy.Create<ISimpleActor>(actorId, "fabric:/MyApplication");
+
+            simpleActor.SetNameAsync("Bob");
+            var name = simpleActor.GetNameAsync();
+
+            Console.WriteLine("Hello, " + name.Result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleActor.Interfaces/ISimpleActor.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+
+namespace SimpleActor.Interfaces
+{
+    /// <summary>
+    ///     This interface represents the actions a client app can perform on an
+    ///     actor. It MUST derive from IActor and all methods MUST return a Task.
+    /// </summary>
+    public interface ISimpleActor : IActor
+    {
+        Task<int> GetCountAsync();
+
+        Task SetCountAsync(int count);
+
+        Task<string> GetNameAsync();
+
+        Task SetNameAsync(string name);
+    }
+}
